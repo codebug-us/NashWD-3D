6e21e15a65203d602542147c1b29d6b5bd043b77
sarah-codebug <sbetack@gmail.com>
2019-04-15 17:31:55 -0500

    add exercise

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# NashWD-3D
-Advanced Functions
+# Conjunction Junction What's Your Function
+
+## In `index.html`
+* Open `index.html` in Sublime
+* Open `index.html` in Chrome
+* Open the developer tools console in Chrome.
+
+## In `script.js`
+### Use the directions below to write your functions in `script.js`
+###### Check each function as you go!
+----
+EXAMPLE FUNCTION (Let's do this one together!)
+* Write a function called `hotdogNotHotdog` that takes in the name of an item and returns "hotdog" if the item is a hotdog and "not hotdog" if the item is not a hotdog. 
+----
+* Write the famous [`helloWorld` function](https://en.wikipedia.org/wiki/%22Hello,_World!%22_program) that returns "Hello World!"
+* Write a function called `yell` that takes in a string and returns the uppercase version of it. [Hint](https://www.w3schools.com/jsref/jsref_touppercase.asp) Javascript has a built in function to help you. 
+* Write a function called `makeMoreExciting` that takes in a string and returns the string with `!!!` added onto the end of it. 
+* Write a function called `getLength` that takes in a string and returns the length of the string. [Hint](https://www.tutorialspoint.com/javascript/string_length.htm) Javascript has a built in function to help you. 
+* Write a function called `rockstarNamer` that takes in the name of your first pet and the name of the street you grew up on. The function should return the string, "Your rockstar name is _____ _____." For example, `rockstarNamer("Snickers", "Cloverdale")` would return, "Your rockstar name is Snickers Cloverdale."
+* Write a function called `combinedLength` that takes in two strings and returns the length of the two strings combined. 
+* Write a function called `bigDaddy` that takes in 2 numbers and returns the larger number.
+* Write a function called `babyDaddy` that takes in 2 numbers and returns the smaller number.
+* Write a function called `breakfastChef`. It should take in two booleans. The first is wantsSalty, the second is wantsSweet. The function should look like this `breakfastChef(wantsSalty, wantsSweet)`. If the person wants sweet food it should return chocolate chip pancakes. If the person wants salty food the function should return a ham and cheese omelette. If the person wants sweet and salty, it should return chicken and waffles. If the person does not want sweet or salty it should return water.
+* Write a function called `tweener` that takes in two numbers and returns a number that is between the two. 
+* Write a function called `countingInNickels` that takes in a number and returns true or false depending on if the number is evenly divisible by 5 or not.
+* Write a function called `capitalPolice` that takes in a string and returns true if the string contains any capital letters and false if the string does not. 
+* Write a function called `isNum` that takes in a string and returns true if the string is a number and false if the string is not a number.
---
+++ script.js
@@ -0,0 +1,171 @@
+document.addEventListener('DOMContentLoaded', function() {
+//////////////////////////////////////////////////////////////////////////
+  // Write the `hotdogNotHotdog` function below.
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(hotdogNotHotdog("hotdog"))
+  // Expected output: hotdog
+  // console.log(hotdogNotHotdog("peyton manning"))
+  // Expected output: not hotdog
+
+//////////////////////////////////////////////////////////////////////////
+  // Write the `helloWorld` function below.
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(helloWorld())
+  // Expected output: Hello World!
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `yell` below. 
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(yell("Party!"))
+  // Expected output: PARTY!
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `makeMoreExciting` below.
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(makeMoreExciting("It's Tuesday"));
+  // Expected output: It's Tuesday!!!
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `getLength` below.
+
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(getLength("How long am I? Too long? Too short?"));
+  // Expected output: 35
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `rockstarNamer` below.
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(rockstarNamer("Snickers", "Cloverdale"));
+  // Expected output: Your rockstar name is Snickers Cloverdale.
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `combinedLength` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log(combinedLength("Blueberry", "Muffins are my homies"));
+  // Expected output: 30
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `bigDaddy` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log(bigDaddy(17, 10));
+  // Expected output: 17
+  // console.log(bigDaddy(100, 200));
+  // Expected output: 200
+  // console.log(bigDaddy(-4, 4));
+  // Expected output: 4
+  // console.log(bigDaddy(21, 21));
+  // Expected output: 21
+  
+//////////////////////////////////////////////////////////////////////////
+  // Write `babyDaddy` below.
+
+
+
+
+  // Uncomment the line below, and look in the console to see if your function worked!
+  // console.log(babyDaddy(17, 10));
+  // Expected output: 10
+  // console.log(babyDaddy(100, 200));
+  // Expected output: 100
+  // console.log(babyDaddy(-4, 4));
+  // Expected output: -4
+  // console.log(babyDaddy(21, 21));
+  // Expected output: 21
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `breakfastChef` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log(breakfastChef(true, true));
+  // Expected output: "chicken and waffles"
+  // console.log(breakfastChef(true, false));
+  // Expected output: "ham and cheese omelette"
+  // console.log(breakfastChef(false, true));
+  // Expected output: "chocolate chip pancakes"
+  // console.log(breakfastChef(false, false));
+  // Expected output: "water"
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `tweener` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log(tweener(1, 4));
+  // Expected output: Your output should be between 1 and 4
+  // console.log(tweener(30, 2));
+  // Expected output: Your output should be between 2 and 30
+  // console.log(tweener(-10, -4));
+  // Expected output: Your output should be between -4 and -10
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `countingInNickels` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log(countingInNickels(20));
+  // Expected output: true
+  // console.log(countingInNickels(23));
+  // Expected output: false
+  // console.log(countingInNickels(0));
+  // Expected output: true
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `capitalPolice` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log(capitalPolice("hey there, how are you doing?"));
+  // Expected output: false
+  // console.log(capitalPolice("Howdy Folks!"));
+  // Expected output: true
+
+//////////////////////////////////////////////////////////////////////////
+  // Write `isNum` below.
+
+
+
+
+  // Uncomment the lines below, and look in the console to see if your function worked!
+  // console.log(isNum("I am not a number"));
+  // Expected output: false
+  // console.log(isNum("11"));
+  // Expected output: true
+
+//////////////////////////////////////////////////////////////////////////
+
+})
